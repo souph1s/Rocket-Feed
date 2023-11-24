6f624df567e3d23ef5be608257a027e217c21906
Sophia <sophiamperez@hotmail.com>
2023-11-24 15:29:08 -0300

    subindo projeto

---
+++ rocket-feed-ts/src/components/Avatar.tsx
@@ -0,0 +1,15 @@
+import { ImgHTMLAttributes } from "react";
+import styles from "./Avatar.module.css";
+
+interface Avatar extends ImgHTMLAttributes<HTMLImageElement> {
+  hasBorder?: boolean;
+}
+
+export function Avatar({ hasBorder = true, ...props}: Avatar) {
+  return (
+    <img
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      {...props}
+    />
+  );
+}
